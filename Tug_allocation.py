eace9137047ca4ce3e817fec4d91b8df0c912503
folkertdk <80403783+folkertdk@users.noreply.github.com>
2025-03-06 15:50:17 +0100

    Test

--- Tug_allocation.py
+++ Tug_allocation.py
@@ -9,7 +9,7 @@ class Tug:
         self.status = "idle"  # Tug status (idle, assigned, busy)
         self.position = position  # Current position (node ID)
         self.path = []  # Path assigned to the tug
-        self.distance_traveled = 0  # Total distance traveled
+        self.distance_traveled = 0  # Total distance traveled; KPI voor later
         self.depot = depot  # Home depot
     
     def assign_task(self, task):
@@ -62,20 +62,41 @@ class FlightTask:
 def generate_flight_task(flight_id):
     """Generates a flight task at a certain frequency."""
     a_d = random.choice(["A", "D"])  # Arrival or Departure
-    start_node = random.randint(1, 10)  # Random start node (example range)
-    goal_node = random.randint(1, 10)  # Random goal node (example range)
+    
+    if a_d == "A":
+        start_node = random.choice([37, 38])  # Arrivals spawn at node 37 or 38
+        goal_node = random.choice([97, 34, 35, 36, 98])  # Arrivals go to one of these nodes
+    else:
+        start_node = random.choice([97, 34, 35, 36, 98])  # Departures spawn at these nodes
+        goal_node = random.choice([1, 2])  # Departures go to either node 1 or 2
+    
     spawn_time = time.time()
     return FlightTask(flight_id, a_d, start_node, goal_node, spawn_time)
 
-# Example setup
-depot = Depot(1, position=5)  # Creating a depot at node 5
+# Creating two depots
+# NOTE Miguel: Deze nodes (node 20 and node 17) gaan we nog veranderen, maar eerst moeten we even de nodes toevoegen aan de Excel file hiervoor
+departure_depot = Depot(1, position=20)  # Departure depot at node 20
+arrival_depot = Depot(2, position=17)  # Arrival depot at node 17
 
-tug1 = Tug(1, position=5, depot=depot)  # Tug at depot
+# Creating tugs for each depot
+tug1 = Tug(1, position=20, depot=departure_depot)  
+tug2 = Tug(2, position=17, depot=arrival_depot)  
 
-depot.add_tug(tug1)  # Adding tug to depot
+departure_depot.add_tug(tug1)  
+arrival_depot.add_tug(tug2) 
 
-task1 = generate_flight_task(1)  # Generating a flight task
+# Generating a flight task
+task1 = generate_flight_task(1) #Voor nu 1 task en 1 tug per depot om te checken
 
-depot.add_task(task1)  # Adding task to depot
+# Assigning tasks to the appropriate depot
+if task1.type == "A":
+    arrival_depot.add_task(task1)
+    arrival_depot.match_task()
+else:
+    departure_depot.add_task(task1)
+    departure_depot.match_task()
 
-depot.match_task()  # Matching a task to a tug
+
+# Als je runt krijg je:
+# Tug 2 assigned to task 1
+# Tug 2 requesting A* path from 17 to 35
