2bf857c1f1152bd12ef5e148c8618a06dddab3da
folkertdk <80403783+folkertdk@users.noreply.github.com>
2025-03-07 17:59:57 +0100

    Restructering, Aircraft class new tug class, Depot logic changed

---
+++ Depot_file.py
@@ -0,0 +1,46 @@
+import random
+import time
+from queue import Queue
+
+
+class Depot:
+    """Depot class to manage tugs and task queue."""
+    def __init__(self, depot_id, position):
+        self.id = depot_id  # Depot ID
+        self.position = position  # Depot position (node ID)
+        self.tugs = Queue()  # FIFO queue for idle tugs
+        self.tasks = Queue()  # FIFO queue for tasks
+
+
+    def add_task(self, task):
+        """Add a task to the depot queue."""
+        self.tasks.put(task)
+
+
+    def match_task(self):
+        """Assign tasks to available tugs."""
+        if not self.tugs.empty() and not self.tasks.empty():
+            tug = self.tugs.get()
+            task = self.tasks.get()
+            tug.assign_task(task)
+            tug.request_path(task)
+
+class FlightTask:
+    """Represents a flight task."""
+    def __init__(self, flight_id, a_d, start_node, goal_node, spawn_time):
+        self.flight_id = flight_id
+        self.type = a_d  # Arrival (A) or Departure (D)
+        self.start_node = start_node
+        self.goal_node = goal_node
+        self.spawn_time = spawn_time
+
+
+
+
+
+
+
+
+# Als je runt krijg je:
+# Tug 2 assigned to task 1
+# Tug 2 requesting A* path from 17 to 35
